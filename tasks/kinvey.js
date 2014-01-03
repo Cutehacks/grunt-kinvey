6df4802eee60106422c985b833cfb1c9249f5dd5
Jason Barron <jason@cutehacks.com>
2014-01-03 09:20:43 +0100

    Silence some jshint warnings.

--- tasks/kinvey.js
+++ tasks/kinvey.js
@@ -12,8 +12,9 @@ module.exports = function(grunt) {
     grunt.registerMultiTask('kinvey', 'A Grunt plugin that wraps the kinvey command line tool for managing business logic.', function() {
         var done = this.async();
 
-        if (!grunt.file.exists('.kinvey'))
+        if (!grunt.file.exists('.kinvey')) {
             grunt.log.error("Grunt file does not exist. Please run 'kinvey init' manually.");
+        }
 
         // Instead of silently ignoring 'files', throw an error so the user does not have false expectations.
         if (this.files.length > 0) {
@@ -45,21 +46,26 @@ module.exports = function(grunt) {
             args.push('--environment', data.environment);
         }
 
-        if (!options.email && !options.password)
+        if (!options.email && !options.password) {
             grunt.log.warn('No email/password specified. Will attempt to use .kinvey credentials.');
+        }
 
-        if (options.email)
+        if (options.email) {
             args.push('--email', options.email);
+        }
 
-        if (options.password)
+        if (options.password) {
             args.push('--password', options.password);
+        }
 
         // TODO: Disabled due to http://support.kinvey.com/hc/communities/public/questions/200667786--BUG-REPORT-kinvey-command-line-tool-does-not-accept-app-argument
-//        if (options.app)
+//        if (options.app) {
 //            args.push('--app', options.app);
+//        }
 
-        if (options.debug)
+        if (options.debug) {
             args.push('--debug', options.debug);
+        }
 
         if (this.errorCount > 0) {
             done(new Error('An error has occured.'));
@@ -69,7 +75,7 @@ module.exports = function(grunt) {
         var child = {
             cmd: 'kinvey',
             args: args
-        }
+        };
 
         if (options.quiet) {
             args.push('--quiet');
@@ -84,8 +90,9 @@ module.exports = function(grunt) {
         });
 
         // even with --quiet option the kinvey script will wait for input so send it a linefeed to continue
-        if (options.quiet && data.command === 'refresh')
+        if (options.quiet && data.command === 'refresh') {
             kinvey.stdin.write(grunt.util.linefeed);
+        }
 
     });
 };
